befd59353cc3bee421b6153eceb5b305ecae32f0
Tony Dubreil <tony.dubreil@niji.fr>
2015-04-22 12:38:45 +0200

    Fix CS.

--- src/Command/Project/TestAllCommand.php
+++ src/Command/Project/TestAllCommand.php
@@ -15,8 +15,8 @@
 
 namespace Jarvis\Command\Project;
 
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Jarvis\Project\ProjectConfiguration;
 
