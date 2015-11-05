49db370e73d1c131471f702ed3f3b9a84e3ebe44
Tony Dubreil <tony.dubreil@niji.fr>
2015-11-05 16:46:03 +0100

    Add a new command "project:git:diff" and refactoring for all services about git commands for project.

--- src/Command/Project/BaseGitCommand.php
+++ src/Command/Project/BaseGitCommand.php
@@ -15,11 +15,86 @@
 
 namespace Jarvis\Command\Project;
 
-use Symfony\Component\Console\Output\OutputInterface;
 use Jarvis\Process\ExecAwareTrait;
 use Jarvis\Project\ProjectConfiguration;
+use Symfony\Component\Console\Output\OutputInterface;
 
-abstract class BaseGitCommand extends BaseCommand
+class BaseGitCommand extends BaseCommand
 {
     use ExecAwareTrait;
+
+    protected $gitCommand;
+
+    protected $colorsConsoleOutput = false;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function executeCommandByProject($projectName, ProjectConfiguration $projectConfig, OutputInterface $output)
+    {
+        $output->writeln(
+            sprintf(
+                '<comment>Git %s for project "<info>%s</info>"</comment>',
+                $this->getName(),
+                $projectName
+            )
+        );
+
+        if (!is_dir($projectConfig->getLocalGitRepositoryDir())) {
+            throw new \RuntimeException(sprintf('The directory "%s" does not exist', $projectConfig->getLocalGitRepositoryDir()));
+        }
+
+        if (empty($this->gitCommand)) {
+            throw new \LogicException('Git command is empty');
+        }
+
+        if ($this->colorsConsoleOutput) {
+            ob_start();
+        }
+
+        $this->getExec()->passthru(
+            sprintf('git %s', $this->gitCommand),
+            $projectConfig->getLocalGitRepositoryDir()
+        );
+
+        if ($this->colorsConsoleOutput) {
+            $output->writeln(
+                strtr(
+                    ob_get_clean(),
+                    [
+                        'up-to-date' => '<info>up-to-date</info>',
+                        'is behind' => '<info>is behind</info>',
+                        'can be fast-forwarded' => '<info>can be fast-forwarded</info>',
+                        'modified:' => '<info>modified:</info>',
+                        'both modified' => '<error>both modified:</error>',
+                        'deleted:' => '<error>deleted:</error>', // red color
+                        'deleted by us' => '<error>deleted by us</error>', // red color
+                        '-  ' => '<error>- </error>', // red color
+                        '+  ' => '<info>+</info>', // green color
+                    ]
+                )
+            );
+        }
+
+        return $this->getExec()->getLastReturnStatus();
+    }
+
+    /**
+     * Sets the value of gitCommand.
+     *
+     * @param string $gitCommand the git command
+     *
+     * @return self
+     */
+    public function setGitCommand($gitCommand)
+    {
+        $this->gitCommand = $gitCommand;
+
+        return $this;
+    }
+
+    public function enableColorsConsoleOutput()
+    {
+        $this->colorsConsoleOutput = true;
+    }
 }
