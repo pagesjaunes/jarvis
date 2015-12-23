95edd39b11a6fbd89b9d66c040a6525da1c05101
Tony Dubreil <tony.dubreil@niji.fr>
2015-12-23 15:41:55 +0100

    Add instruction to execute when bumped version is finished

--- bin/bump-version.sh
+++ bin/bump-version.sh
@@ -74,5 +74,6 @@ git commit -m "Bump version ${TAG}"
 git checkout master
 
 echo "New version created. Now you should run:"
+echo "git push origin master"
 echo "git push origin gh-pages"
 echo "git push --tags"
